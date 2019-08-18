b9fe51c6a95c83cab378bba5b4facd3561203389
Ricram2 <ricram2@gmail.com>
2019-08-18 01:03:55 -0400

    Adding Setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='Jinou_Humi_Temp_Reader',
+    version='1.0',
+    packages=[''],
+    url='',
+    license='Mit ',
+    author='ricram2',
+    author_email='ricram2@gmail.com',
+    description='Sensor reading for humidity and temperature for JINOU Beacon. JO-BEC09 Python code tested in Headless Raspbian'
+)
